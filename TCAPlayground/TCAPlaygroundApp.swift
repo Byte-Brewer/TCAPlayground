871715c1e6f7fda47c3b30e82dc6cdd6db1e51bc
NazarPrysiazhnyi <2008Nazar@gmail.com>
2023-08-26 23:28:28 +0300

    feat [init]: The project has been created

---
+++ TCAPlayground/TCAPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPlaygroundApp.swift
+//  TCAPlayground
+//
+//  Created by Nazar Prysiazhnyi on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
